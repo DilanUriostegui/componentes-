c5e401f2428924f3d8dbaf1ff0421bd8f502a3ac
Dilan <duriostegui@asgbpm.com>
2021-04-09 14:18:56 -0500

    FutureBuilder: Creación de la lista a partir de un archivo JSON

--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/services.dart' show rootBundle;
 
+import 'dart:convert';
 
 class _MenuProvider{
 
   List<dynamic> opciones = [];
 
   _MenuProvider(){
-    cargarData();
+    //cargarData();
   }
 
-  void cargarData() {
+  Future <List<dynamic>> cargarData() async {
 
 
-    rootBundle.loadString('data/menu_opts.json')
-      .then( (data) {
+    final resp = await rootBundle.loadString('data/menu_opts.json');
+      
+    Map dataMap = json.decode(resp);
+    opciones = dataMap['rutas'];
+    
+    return opciones;
 
-        print(data);
 
-      });
+
 
   }
 
