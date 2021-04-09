6ee19833a9329189ce8ca79cea117a8d95f9fb1a
Dilan <duriostegui@asgbpm.com>
2021-04-09 11:52:06 -0500

    Implementando el metodo map y más funciones de ListTile

--- lib/src/home_temp.dart
+++ lib/src/home_temp.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
 
+  final opciones = {'Uno','Dos','Tres','Cuatro','Cinco'};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,21 +13,44 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Componentes Temp'),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-        ],
+        //children: _crearItems()
+        children: _crearItemsCorta() 
       ),
 
     );
+  }
+
+
+  List<Widget> _crearItems(){
+
+    List<Widget> lista = new List<Widget>();
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+    lista..add(tempWidget)
+         ..add(Divider());
+    
+    }
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta(){
+
+    return opciones.map(( item ){
+
+      return Column(
+        children: <Widget>[
+          ListTile(
+            title: Text( item + '!'),
+            subtitle: Text('Cualquier cosa'),
+            leading: Icon(Icons.account_balance_wallet),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: (){ },
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
   }
 }
