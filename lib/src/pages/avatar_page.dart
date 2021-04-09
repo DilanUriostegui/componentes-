009ad5c95909cea37b493048e33cccfe130edfe2
Dilan <duriostegui@asgbpm.com>
2021-04-09 18:31:54 -0500

    Se agregó la pagina con Avatars

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,8 +6,35 @@ class AvatarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Avatar Page')
+        title: Text('Avatar Page'),
+        actions: [
+
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://culturageek.com.ar/wp-content/uploads/2019/06/615970090.jpg.0.jpg'),
+              radius: 25.0,
+            ),
+          ),
+
+
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
       ),
+      body: Center(
+        child: FadeInImage(
+          
+          image: NetworkImage('https://asisoytv.com/wp-content/uploads/2018/11/stanleeeejpg-52d6ae_1280w.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      )
     );
   }
 }
