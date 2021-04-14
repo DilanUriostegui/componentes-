3053fe5f808a2505860f98d4860289cdac2cd68b
Dilan <duriostegui@asgbpm.com>
2021-04-14 13:56:58 -0500

    En inpunts se agregó el Datepicker para la fecha y un Dropdown

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 //
 import 'package:componentes/src/routes/routes.dart';
 import 'package:componentes/src/pages/cart_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 
 
 
@@ -17,6 +19,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''), // English, no country code
+        const Locale('es', ''), // Spanish, no country code
+      ],
       //home: HomePageTemp()
       //home: HomePage(),
       initialRoute: '/',
--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -9,6 +9,12 @@ class _InputPageState extends State<InputPage> {
 
   String _nombre = '';
   String _email = '';
+  String _fecha = '';
+  String _opcionSeleccionada = 'Volar';
+
+  List<String> _poderes = ['Volar', 'Rayos X', 'Super Aliento', 'Super fuerza'];
+
+  TextEditingController _inputFieldDateController = TextEditingController();
 
 
 
@@ -26,6 +32,10 @@ class _InputPageState extends State<InputPage> {
           _crearEmail(),
           Divider(),
           _crearPassword(),
+          Divider(),
+          _crearFecha(context),
+          Divider(),
+          _crearDropdown(),
           Divider(),
           _crearPersona(),
         ],
@@ -58,15 +68,9 @@ class _InputPageState extends State<InputPage> {
       },
     );
   }
-Widget _crearPersona(){
-    return ListTile(
-      title: Text('Nombre es: $_nombre'),
-      subtitle: Text('Email: $_email'),
-    );
-  }
   
-Widget _crearEmail(){
-  return TextField(
+  Widget _crearEmail(){
+    return TextField(
       keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
         border: OutlineInputBorder(
@@ -78,15 +82,15 @@ Widget _crearEmail(){
         icon: Icon(Icons.email),
       ),
       onChanged: (valor) => setState(() {
-          _email = valor;  
-        })
+        _email = valor;  
+      })
     );
 
-}
+  }
 
 
-Widget _crearPassword(){
-  return TextField(
+  Widget _crearPassword(){
+    return TextField(
       obscureText: true,
       decoration: InputDecoration(
         border: OutlineInputBorder(
@@ -98,12 +102,94 @@ Widget _crearPassword(){
         icon: Icon(Icons.lock),
       ),
       onChanged: (valor) => setState(() {
-          _email = valor;  
-        })
+        _email = valor;  
+      }
+      )
     );
+  }
 
+
+  Widget _crearFecha(BuildContext context){
+    return TextField(
+      enableInteractiveSelection: false,
+      controller: _inputFieldDateController,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        hintText: 'Fecha de nacimiento',
+        labelText: 'Fecha de nacimiento',
+        suffix: Icon(Icons.perm_contact_calendar),
+        icon: Icon(Icons.calendar_today),
+      ),
+      onTap: (){
+        FocusScope.of(context).requestFocus(FocusNode());
+        _selectDate(context);
+      },
+    );
+  }
+
+  _selectDate(BuildContext context) async{
+    DateTime picked = await showDatePicker(
+      context: context, 
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2018),
+      lastDate: DateTime(2025),
+      locale: Locale('es','ES')
+      );
+
+      if(picked != null){
+        setState(() {
+          _fecha = picked.toString();
+          _inputFieldDateController.text = _fecha;
+        });
+      }
+  }
+
+  List<DropdownMenuItem<String>> getOpcionesDropdown(){
+    List<DropdownMenuItem<String>> lista = [];
+
+    _poderes.forEach((poder) {
+      
+      lista.add(DropdownMenuItem(
+        child: Text(poder),
+        value: poder,
+      ));
+
+    });
+    return lista;
+  }
+
+  Widget _crearDropdown(){
+    return Row(
+      children: [
+        Icon(Icons.select_all),
+        SizedBox(width: 30.0),
+        Expanded(
+          child: DropdownButton(
+            value: _opcionSeleccionada,
+            items: getOpcionesDropdown(),
+            onChanged: (opt){
+              setState(() {
+                _opcionSeleccionada = opt;
+              });
+            },
+          ),
+        )
+      ],
+    );
+    
+    
+    
+  }
+
+
+  Widget _crearPersona(){
+    return ListTile(
+      title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email: $_email'),
+      trailing: Text(_opcionSeleccionada),
+    );
 }
-
-
 
 }
