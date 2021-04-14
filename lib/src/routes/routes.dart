299163923ebfaf8b141a8046d17918e1a2ba0a4e
Dilan <duriostegui@asgbpm.com>
2021-04-14 12:26:08 -0500

    Se agrego la pagina de AnimatedContaines y de Inputs

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,6 +5,9 @@ import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/cart_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/input_page.dart';
+
 
 
 
@@ -14,6 +17,8 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
         'alert' : ( BuildContext context ) =>AlertPage(),
         'avatar' : ( BuildContext context ) => AvatarPage(),
         'card' : ( BuildContext context ) =>CardPage(),
+        'animatedContainer' : ( BuildContext context ) =>AnimatedContainerPage(),
+        'inputs' : ( BuildContext context ) =>InputPage(),
   };
 
 }
