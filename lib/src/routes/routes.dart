2e9066a0735639946943235635946fc0f9e63dab
Dilan <duriostegui@asgbpm.com>
2021-04-14 14:20:04 -0500

    Creación de Sliders

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -7,6 +7,8 @@ import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/cart_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/slider_page.dart';
+
 
 
 
@@ -19,6 +21,7 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
         'card' : ( BuildContext context ) =>CardPage(),
         'animatedContainer' : ( BuildContext context ) =>AnimatedContainerPage(),
         'inputs' : ( BuildContext context ) =>InputPage(),
+        'slider' : ( BuildContext context ) =>SliderPage(),
   };
 
 }
